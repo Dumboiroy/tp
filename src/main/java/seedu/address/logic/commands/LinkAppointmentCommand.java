933bfff68c311168c10ae97ba00fc5403cbbcf58
Zhao Rui <zhaorui031202@gmail.com>
2025-10-12 18:13:37 +0800

    Add more help message

--- src/main/java/seedu/address/logic/commands/LinkAppointmentCommand.java
+++ src/main/java/seedu/address/logic/commands/LinkAppointmentCommand.java
@@ -8,19 +8,8 @@ import seedu.address.model.Model;
  */
 public abstract class LinkAppointmentCommand extends Command {
     public static final String COMMAND_WORD = "link";
-
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Links a new appointment to a client. "
-            + "Parameters: "
-            + "FLAG "
-            + "/n CLIENT_NAME "
-            + "[/id ID]"
-            + "/appt DATE [TIME] "
-            + "[/len MINUTES] "
-            + "[/loc LOCATION] "
-            + "[/type TYPE] "
-            + "[/msg NOTES] "
-            + "[/status planned|confirmed|completed|cancelled]\n"
-            + "Example: " + COMMAND_WORD + " -c "
+    public static final String MESSAGE_EDIT_APPOINTMENT = COMMAND_WORD + " -e "
+            + "/id 123456 "
             + "/n Alex Wu "
             + "/appt 12-10-2025 1430 "
             + "/len 90 "
@@ -28,7 +17,28 @@ public abstract class LinkAppointmentCommand extends Command {
             + "/type home-visit "
             + "/msg Bring consent form "
             + "/status planned";
-
+    public static final String MESSAGE_ADD_APPOINTMENT = COMMAND_WORD + " -c "
+            + "/n Alex Wu "
+            + "/appt 12-10-2025 1430 "
+            + "/len 90 "
+            + "/loc Bukit Merah FSC "
+            + "/type home-visit "
+            + "/msg Bring consent form "
+            + "/status planned";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Links a new appointment to a client. "
+            + "Parameters: "
+            + "FLAG "
+            + "/n CLIENT_NAME "
+            + "[/id ID] "
+            + "/appt DATE [TIME] "
+            + "[/len MINUTES] "
+            + "[/loc LOCATION] "
+            + "[/type TYPE] "
+            + "[/msg NOTES] "
+            + "[/status planned|confirmed|completed|cancelled]\n"
+            + "Example:\n"
+            + MESSAGE_ADD_APPOINTMENT + "\n"
+            + MESSAGE_EDIT_APPOINTMENT;
     public static final String MESSAGE_SUCCESS = "New appointment linked to %1$s: %2$s";
     public static final String MESSAGE_NO_SUCH_PERSON = "No client found with the name: %1$s";
     public static final String MESSAGE_DUPLICATE_APPOINTMENTS = "This appointment already exists in the address book.";
